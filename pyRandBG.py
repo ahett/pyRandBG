e5e52b6cf893853a1d36cabaee126d30a9bac38d
Adrian Hett <adrian@hett.cc>
2017-04-11 17:33:01 +0200

    arranged in functions

--- pyRandBG.py
+++ pyRandBG.py
@@ -1,27 +1,43 @@
 import os
 import sys
 import subprocess
+import time
 from random import randint
-
-if len(sys.argv) <= 1:
-    print('no argumemts')
-    indir = './'
-else:
-    indir = sys.argv[1]
-
-print('directory:', indir)
-
-files = os.listdir(indir)
-numFiles = len(files)
-
-chosenFiles = [indir + files[randint(0, numFiles - 1)],
-               indir + files[randint(0, numFiles - 1)],
-               indir + files[randint(0, numFiles - 1)],
-               indir + files[randint(0, numFiles - 1)]]
+from PIL import Image, ImageFilter
 
 
-singleChosenFull = chosenFiles[0];
-print(singleChosenFull)
-command = "gconftool-2 --set /desktop/gnome/background/picture_filename --type string '" + singleChosenFull + "'"
-status = os.system(command)
-print (status)
+def get_random_files():
+    indir = '/home/adrian/Bilder/Wallpaper/'
+    files = os.listdir(indir)
+    num_files = len(files)
+
+    ret = [indir + files[randint(0, num_files - 1)],
+           indir + files[randint(0, num_files - 1)],
+           indir + files[randint(0, num_files - 1)],
+           indir + files[randint(0, num_files - 1)]]
+
+    return ret
+
+
+def create_spanning(files):
+
+    im1 = Image.open(files[0]).resize(2560,1440)
+    im2 = Image.open(files[1]).resize(2560,1440)
+    im3 = Image.open(files[2]).resize(2560,1440)
+    im4 = Image.open(files[3]).resize(1920,1200)
+
+    ret=Image.new(9600,1440)
+    return files[0]
+
+
+def set_background(file):
+    command = " gsettings set org.gnome.desktop.background picture-uri file://" + file + ""
+    print(command)
+    status = os.system(command)
+    print(status)
+
+while True:
+    files = get_random_files()
+    spanned = create_spanning(files)
+    set_background(spanned)
+    time.sleep(30)
